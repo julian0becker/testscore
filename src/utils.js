477da29b1b98b9467da1d2301206d51b85fcc907
julian0becker <42543792+julian0becker@users.noreply.github.com>
2019-05-21 12:18:42 +0200

    Delete utils.js

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-export function findIndexStudent(studentArray, idStudent) {
-  return studentArray.findIndex(student => student.studentId === idStudent);
-}
-
-export function findIndexTest(studentArray, indexStudent, idTest) {
-  return studentArray[indexStudent].results.findIndex(
-    test => test.testId === idTest
-  );
-}
